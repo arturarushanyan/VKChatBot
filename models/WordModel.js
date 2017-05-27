2a08095e1f229c22e03922edd195f0b18037deba
arturarushanyan <arthur.arushanyan92@gmail.com>
2017-05-27 19:45:26 +0400

    adding stuff

---
+++ models/WordModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+let wordSchema = mongoose.Schema({
+    word: String,
+    translation: String
+
+},{
+    versionKey: false,
+    strict: false
+});
+
+let WordModel = mongoose.model('Words', wordSchema);
+
+
+
+const mongoWord = {
+
+    addWord: (msg) => {
+        WordModel.create({
+                word: msg.word,
+                translation: msg.translation
+            },
+            (err,result) => {
+                if (err){
+                    throw err;
+                } else {
+                    console.log('Word Created!'+result);
+                }
+            });
+    },
+
+    getWord: (msg, next) => {
+        WordModel.findOne({word: msg.word}, null, {lean :true}, (err,result) => {
+            if(err){
+                throw err;
+            } else {
+                next(null, result);
+            }
+        });
+    },
+
+    getAll: () => {
+        WordModel.find();
+    }
+};
+
+module.exports = mongoWord;
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const VK = require('vk-io');
+const mongoose = require('mongoose');
+const mongoWord = require('./models/WordModel');
 
 const vk = new VK({
     token: "098a6b6ec7ba99092ad8d2bcb2cce59630e4fdcaa57d49a84aae31661cf718ad245d786fdb2ceeaa099a3"
 });
 
+mongoose.connect("mongodb://artur:artur@ds151661.mlab.com:51661/heroku_b0rdttvx");
+
+const db = mongoose.connection;
+
+db.on('error',() => console.error("hello error"));
+
+db.once('open',() => console.log("server opened.."));
 
 vk.longpoll.start()
     .then(() => {
@@ -15,10 +24,25 @@ vk.longpoll.start()
 
 
 vk.longpoll.on('message', (msg) => {
-    if(msg.flags.indexOf('outbox') != -1){
+    if(msg.flags.indexOf('outbox') !== -1){
         return;
     }
-    console.log(msg);
-    msg.send(msg.text);
+
+    let parsedMessage = msg.text.split(" ");
+    let message = {
+        word: parsedMessage[1],
+        translation: parsedMessage[2]
+    };
+    if(parsedMessage[0] === "add"){
+        mongoWord.addWord(message);
+        console.log(message);
+    } else if(parsedMessage[0] === "get" && parsedMessage[0] !== "all"){
+        mongoWord.getWord(message, (err, doc) => {
+            console.log(doc);
+            if (!err) msg.send(`your word is ${doc.translation}`)
+        });
+    }
+
+    // msg.send("your message is: "+msg.text);
 
 });
